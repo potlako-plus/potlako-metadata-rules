c62d882617470410234f9186078d86bd9769d803
tumiemosweu <imosweuk@gmail.com>
2020-06-04 16:13:05 +0200

    add rule groups

---
+++ potlako_metadata_rules/subject_rule_groups/patient_call_initial_rules.py
@@ -0,0 +1,19 @@
+from edc_constants.constants import YES
+from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, P, register
+
+app_label = 'potlako_subject'
+
+
+@register()
+class PatientCallInitialRuleGroup(CrfRuleGroup):
+
+    transport = CrfRule(
+        predicate=P('transport_support', 'eq', YES),
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.transport'])
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.patientcallinitial'
